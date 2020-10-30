ff718431bd92fc931f727794f5f7b7dde6e72d9a
Philip <philip@omnimedicalmarketing.com>
2020-10-30 12:50:16 -0600

    Added status page code files for those that want to use it. Module version coming soon!

--- README.md
+++ README.md
@@ -23,3 +23,8 @@ The `css/custom.css` contains customizations such as removing module headers and
 The `config/config.js` contains my setup for each module.
 
 _My collection of backgrounds used is available for download [here](https://drive.google.com/file/d/1pKJM75EsiSegkv3AJiF6Wgw1kcQ0xOI5/view?usp=sharing)_
+
+### Status Page
+If you want to use the widget showing online/offline status of services such as sabnzbd, plex, etc. You'll have to edit `status/index.php` and upload it all to a webserver and use the [MMM-iFrame](https://github.com/alberttwong/MMM-iFrame) module to show it on your dashboard.
+
+As I wrote this, I decided it would be a fun idea to convert that to an actual module, so stay tuned!
---
+++ status/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Servers Status</title>
+<meta content="text/html" charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <section class="server-status">
+    <div class="container-fluid">
+      <?php
+      $data = "";
+      //configure script
+      $timeout = "1";
+      $services = array();
+      $services[] = array("port" => "2784", "service" => "Sabnzbd", "icon" => "sab", "ip" => "");
+      $services[] = array("port" => "8989", "service" => "Sonarr", "icon" => "sonarr", "ip" => "");
+      $services[] = array("port" => "7878", "service" => "Radarr", "icon" => "radarr", "ip" => "");
+      $services[] = array("port" => "32400", "service" => "Plex", "icon" => "plex", "ip" => "");
+
+      foreach ($services  as $service) {
+        if($service['ip']==""){
+           $service['ip'] = "192.168.1.10";
+        }
+        $data .= '<div class="row"><div class="col name ' . $service['icon'] . '">' . $service['service'] . '</div>';
+      	$fp = @fsockopen($service['ip'], $service['port'], $errno, $errstr, $timeout);
+      	if (!$fp) {
+        	$data .= '<div class="col status red">Offline</div></div>';
+          //fclose($fp);
+        } else {
+        	$data .= '<div class="col status green">Online</div></div>';
+        	fclose($fp);
+      	}
+      }
+      echo $data;
+      ?>
+    </div>
+  </section>
+</body>
+</html>
